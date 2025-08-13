1d964fbc8d3a96fd1e08526f90f63a4eb8da3717
Adio Mubarak <adiomubby@gmail.com>
2025-08-13 23:19:53 +0100

    Removed console.log from greet function

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+
+export function Header (){
+
+    return(
+        <header>
+            <h1>My First React App</h1>
+            <nav>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Service</a></li>
+                <li><a href="">Contat Us</a></li>
+            </nav>
+        </header>
+    )
+}
+export default Header
